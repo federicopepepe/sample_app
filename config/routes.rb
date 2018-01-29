1e99244c493138d466579fae778e877b2b3981ab
sakyo <tksakyy.0108@example.com>
2018-01-29 04:26:21 +0000

    ad a static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'staticpages/help'
+
+    root'application#hello'
+    
   get 'staticpages/home'
 
   get 'staticpages/help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root'application#hello'
 end
